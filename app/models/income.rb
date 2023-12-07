f3030cd41b4726a58dc750dcf1637c29eb725753
Arvind Mehra <arvind.mehra@smartpension.co.uk>
2023-12-07 21:53:28 +0000

    user login form formatted

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,4 +2,8 @@ class Income < ApplicationRecord
   belongs_to :income_expenditure_statement
 
   DEFAULT_INCOME_CATEGORIES = %w[salary overtime housing_benefits].freeze
+
+  def basic_fields
+    self.fields.where('field_type = ?', 'basic')
+  end
 end
